234d2cec89c60204c9a200ad7e99b12a1fde26b2
Edgar Rosas <gary.blurk@gmail.com>
2021-09-29 20:25:30 -0700

    create tasks

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,5 +1,6 @@
 import { Task, TaskStatus } from './task.model';
 import { Injectable } from '@nestjs/common';
+import { v4 as uuid } from 'uuid';
 
 @Injectable()
 export class TasksService {
@@ -11,11 +12,12 @@ export class TasksService {
 
   createTask(title: string, description: string): Task {
     const task: Task = {
-      id: '',
+      id: uuid(),
       title,
       description,
       status: TaskStatus.OPEN,
     };
+    this.tasks.push(task);
     return task;
   }
 }
